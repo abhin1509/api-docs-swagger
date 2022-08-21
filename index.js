11474f1d62ae9ba53043e1a4195e751cd75a4ed2
Abhinav Kumar <abhin1509@gmail.com>
2022-08-21 20:42:01 +0530

    updated index.js file

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(express.json());
+
+let fruits = [
+  {
+    id: "11",
+    name: "banana",
+    price: 20,
+  },
+  {
+    id: "22",
+    name: "apple",
+    price: 90,
+  },
+  {
+    id: "33",
+    name: "mango",
+    price: 50,
+  },
+];
+
+app.get("/api/v1/hello", (req, res) => {
+  res.send("hello from abhinav");
+});
+
+app.get("/api/v1/mart", (req, res) => {
+  res.send({ id: "55", name: "guava", price: 30 });
+});
+
+app.get("/api/v1/fruits", (req, res) => {
+  res.send(fruits);
+});
+
+app.get("/api/v1/allFruits/:fruitId", (req, res) => {
+  const myFruit = fruits.find((fruit) => fruit.id === req.params.fruitId);
+  res.send(myFruit);
+});
+
+app.post("/api/v1/addFruit", (req, res) => {
+  console.log(req.body);
+  fruits.push(req.body);  // Before pushing, do proper validation check
+  res.send(true);
+});
+
+app.get("/api/v1/stringQuery", (req, res) => {
+  let location = req.query.location;
+  let device = req.query.device;
+  res.send({location, device});
+});
+
+app.listen(3000, () => console.log(`Server is running at port 3000...`));
